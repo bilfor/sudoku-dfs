533c8171ce920ca1837539bd1a9f6198d7dcf7f9
billy <wbfore98@gmail.com>
2022-03-04 19:54:36 -0500

    half pseudo, inference

--- SudokuSolver_Billy.java
+++ SudokuSolver_Billy.java
@@ -1,26 +1,34 @@
 public class SudokuSolver_Billy implements SudokuSolver {
 
   public int[][] solve_dfs(int i, int j, int[][] cells) {
-    int[] empty_coords;
-    //check_complete();
-    empty_coords = find_empty(i,j,cells);
+    int[] coords = find_empty(i,j,cells);
+    if(coords[0] == 10 && coords[1] == 10) return cells; 
     for(int val = 1; val < 10; val++) {
-      if (check_move(empty_coords[0],empty_coords[1],val,cells) == true) {
+      if(check_move(i,j,val,cells)) {
         cells[i][j] = val;
-        break;
+        //inferences <- INFERENCE(csp,var,assignment)
+        //if (inferences != failure) {
+        //  add inferences to csp
+        //  result <- solve_dfs(csp,assignment)
+        //  if (result != failure)
+        //    return result
+        //    remove inferences from csp
+        //    remove {var=value} from assignment
+    //return failure
       }
-    }
-    solve_dfs(empty_coords[0],empty_coords[1],cells);
+      break;
+    } //unroll
+    solve_dfs(i,j,cells);
     return cells;
   }
   
-  public int[] find_empty(int i, int j, int[][] cells) {
+  private int[] find_empty(int i, int j, int[][] cells) {
     int[] coords = new int[2];
     for(int row = i; row < 9; row++) {
       for(int col = j; col < 9; col++) { 
         if(cells[row][col] == 0) {
-          coords[0] = i;
-          coords[1] = j; 
+          coords[0] = row;
+          coords[1] = col; 
           return coords;
         }
       }
@@ -29,6 +37,7 @@ public class SudokuSolver_Billy implements SudokuSolver {
     coords[1] = 10;
     return coords;
   }
+  
 
   public boolean check_move(int i, int j, int val, int[][] cells) {
  
